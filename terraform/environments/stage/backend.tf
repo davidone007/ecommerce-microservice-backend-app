9972ab16bfd60b1379e26f049ee289e4628501dd
andrescabezas26 <pipecabezas00@gmail.com>
2025-11-20 19:29:21 -0500

    feat: add SonarQube module and update storage account names in backend configurations

--- terraform/environments/stage/backend.tf
+++ terraform/environments/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate40259"
+    storage_account_name = "tfstate30989"
     container_name       = "tfstate"
     key                  = "stage.terraform.tfstate"
   }
