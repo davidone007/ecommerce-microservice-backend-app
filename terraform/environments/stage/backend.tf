60e663239cf37812eb6697d0a86d6a548ce3044c
andrescabezas26 <pipecabezas00@gmail.com>
2025-11-22 10:25:20 -0500

    feat: update backend storage account name and change API Gateway service type to LoadBalancer

--- terraform/environments/stage/backend.tf
+++ terraform/environments/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate41265"
+    storage_account_name = "tfstate12736"
     container_name       = "tfstate"
     key                  = "stage.terraform.tfstate"
   }
