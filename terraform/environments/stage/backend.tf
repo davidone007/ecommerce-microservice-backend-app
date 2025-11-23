1b1d426c4cf9cefab368c01e9a56aa7a3f8972d9
andrescabezas26 <pipecabezas00@gmail.com>
2025-11-23 13:33:23 -0500

    fix: update storage account name in Terraform backend configuration for production and staging environments

--- terraform/environments/stage/backend.tf
+++ terraform/environments/stage/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate12736"
+    storage_account_name = "tfstate23280"
     container_name       = "tfstate"
     key                  = "stage.terraform.tfstate"
   }
